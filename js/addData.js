6f23f36bb0d126c317a4ee795324d043f5c81bad
Joseph Harding <jpharding23@gmail.com>
2018-11-16 11:59:57 -0800

    Update js/addData.js

--- js/addData.js
+++ js/addData.js
@@ -1,13 +1,12 @@
 const form = document.querySelector('#add-user-input');
 form.addEventListener('submit', function(evt) {
-evt.preventDefault();
-db.collection('userInput').add({
-createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-name: form.name.value,
-email: form.email.value,
-message: form.message.value
-});
-form.name.value = '';
-form.email.value = '';
-form.message.value = '';
-})
+  evt.preventDefault();
+  db.collection('userInput').add({
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
+    });
+    form.name.value = '';
+    form.email.value = '';
+    form.message.value = '';
+  });
